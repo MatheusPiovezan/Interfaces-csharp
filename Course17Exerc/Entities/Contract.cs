690c645217c23e1bd0b88f0fff933bbfcce7190e
Matheus Piovezan <matheuspiovezan@outlook.com>
2023-09-11 16:59:27 -0300

    adding

---
+++ Course17Exerc/Entities/Contract.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Entities
+{
+    internal class Contract
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+        public List<Installment> ListInstallments { get; set; }
+
+        public Contract(int number, DateTime date, double totalValue)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+            ListInstallments = new List<Installment>();
+        }
+
+        public void AddInstallment(Installment installment)
+        {
+            ListInstallments.Add(installment);
+        }
+    }
+}
